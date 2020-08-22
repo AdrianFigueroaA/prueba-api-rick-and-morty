8296b6746ad3bfa20b2ae34facf040ca44d27774
Adrian <revanskyw@gmail.com>
2020-08-22 14:45:31 -0400

    se agrega funcionalidad ocultar mostrar

--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ import $ from 'jquery'
 
 
 rickMorty.then((personajes) => {
-    const person  = document.getElementById('personajes')
-    personajes.forEach(e => {
-        person.innerHTML +=  `
-        
-                                <div class="card" ;">
-                                <img id="${e.id}" src="${e.imagen}" class="card-img imagen" alt="image">
-                                <div class="card-body  mostrar  ${e.id}" ">
-                                <h5 class="card-title">${e.nombre}</h5>
+    const print  = document.getElementById('personajes')
+personajes.forEach(e => {
+        print.innerHTML +=  `<div class="card " id="${e.id}">
+                             <img  src="${e.imagen}" class="card-img imagen" alt="image">
+                                <div class="card-body ocultar ">
+                                <h5 class="card-title"id="${e.id}">${e.nombre}</h5>
                                 <p class="card-text">${e.genero}</p>
                                 <p class="card-text"><small class="text-muted">${e.especies}</small></p>
                                 <p class="card-text"><small class="text-muted">${e.estado}</small></p>
@@ -20,44 +18,69 @@ rickMorty.then((personajes) => {
                             </div>`
         
     });
+
+
+
+    $('.card').click((e)=>{
+        console.log(e.target)
+        
+        if ( $(`#${e.target.id} .card-body`).hasClass("ocultar")) {
+
+            $(`#${e.target.id} .card-body`).removeClass("ocultar")
+            $(`#${e.target.id} .card-body`).addClass("mostrar")
+            
+        } else {
+
+            $(`#${e.target.id} .card-body`).removeClass("mostrar")
+            $(`#${e.target.id} .card-body`).addClass("ocultar")
+        }
+       
+
+        
+     });
     
-    // $('.imagen').click((e)=>{
-    //    $(`.${e.target.id}`).css('display', 'none')
-    // });
-    
-//         $('.imagen').click((e)=>{
-// console.log(e.target)
-//         //    if (e.target){
-//         //       $(`.${e.target.id}`).css("display", "block");
-//         //    }else{
-//         //       $(`.${e.target.id}`).css("display", "none");
-//         //    }
-//         });
-     
 
 
-// $('.imagen').click((e)=>{
-//     $(`.${e.target.id}`).css("display", "none");
-//   });
-
-// $('.imagen').click((e)=>{
-//     $(`.${e.target.id}`).css("display", "block");
-//   });
-  
-
-
-
-    // $('.imagen').click(function(e){
-    //   $(`.${e.target.id}`).hide();
-    // });
-
-    $('.imagen').click(function(e){
-        $(`.${e.target.id}`).show();
-      });
-
-
-  
    
+//     let seleccion = document.getElementsByClassName("card");
 
+
+//     for(let a of seleccion) {
+//         a.addEventListener("click", elegir(a.id))
+//       }
+    
+      
+
+// function elegir(a){
+
+// console.log(a)
+//     // document.getElementById(a).getElementsByClassName( 'card-body').addClass("mostrar");
+
+// }
+
+
+
+
+// document.getElementsByClassName("card").addEventListener("click", function(e)
+    
+// // document.getElementById(e.target.id ).getElementsByClassName( 'card-body').addClass("mostrar");
+
+// });
+
+
+
+    
+
+    //   $('.imagen').click(function(e){
+
+    //     if ('ocultar') {
+            
+    //         $('.card-body').addClass("mostrar");
+    //       } else {
+    //         $('.card-body').addClass("ocultar");
+    //       }
+    //   });
+
+   
 
 }).catch((error) => console.log(error))
